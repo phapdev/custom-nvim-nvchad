716746bdba69d60023093f8af18086df320526ae
phapdev <luongphap1810@gmail.com>
2024-12-11 10:03:49 +0700

    disable noice.vim

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,41 +1,57 @@
-return {
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      routes = {
-        {
-          filter = { event = "notify", find = "No information available" },
-          opts = { skip = true },
-        },
-      },
-      presets = {
-        lsp_doc_border = true,
-      },
-      notify = {
-        enabled = false,
-      },
-
-      lsp = {
-        progress = {
-          enabled = true,
-        },
-        signature = {
-          enabled = false,
-        },
-        message = {
-          enabled = false,
-        },
-      },
-    },
-
-    dependencies = {
-      --         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --`nvim-notify` is only needed, if you want to use the notification view.
-      --                         --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    },
-  },
-}
+return {}
+-- return {
+--   {
+--     "folke/noice.nvim",
+--     event = "VeryLazy",
+--     opts = {
+--       cmdline = {
+--         enabled = true,
+--         view = "cmdline",
+--       },
+--       message = {
+--         enabled = false,
+--       },
+--       popupmenu = {
+--         enabled = false,
+--       },
+--       routes = {
+--         {
+--           filter = { event = "notify", find = "No information available" },
+--           opts = { skip = true },
+--         },
+--       },
+--       override = {
+--         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--         ["vim.lsp.util.stylize_markdown"] = true,
+--         ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+--       },
+--       presets = {
+--         lsp_doc_border = true,
+--       },
+--       notify = {
+--         enabled = false,
+--       },
+--
+--       lsp = {
+--         progress = {
+--           enabled = true,
+--         },
+--         signature = {
+--           enabled = false,
+--         },
+--         message = {
+--           enabled = false,
+--         },
+--       },
+--     },
+--
+--     dependencies = {
+--       --         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+--       "MunifTanjim/nui.nvim",
+--       -- OPTIONAL:
+--       --`nvim-notify` is only needed, if you want to use the notification view.
+--       --                         --   If not available, we use `mini` as the fallback
+--       "rcarriga/nvim-notify",
+--     },
+--   },
+-- }
